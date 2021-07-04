6feb4ac704ec6941f27d2cc4070aba7ebb0c9960
Min ho Kim <minho42@gmail.com>
2021-07-05 02:07:49 +1000

    add sentry

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const Currency = require("./models/currency");
 const cron = require("node-cron");
 const fetchAll = require("./fetchAll");
 const helmet = require("helmet");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 
 cron.schedule(
   "*/10 * * * *",
@@ -19,6 +21,28 @@ cron.schedule(
 );
 
 const app = express();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(helmet());
@@ -41,10 +65,25 @@ app.get("/data", async (req, res) => {
   }
 });
 
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
 app.get("*", (req, res) => {
   res.status(404).send({
     error: "404 Not Found",
   });
+});
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
 });
 
 app.listen(port, () => {
