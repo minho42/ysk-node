cfdaf62a81b86e11ac5084e8df7f0186198bc09d
Min ho Kim <minho42@gmail.com>
2022-08-31 12:17:45 +1000

    remove mongodb atlas

--- src/db/connectToDb.js
+++ src/db/connectToDb.js
@@ -1,10 +1,7 @@
-import dotenv from "dotenv";
-dotenv.config();
 import mongoose from "mongoose";
 
 export const connectToDb = () => {
-  const mongodbUri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ysk-api";
-  mongoose.connect(mongodbUri, {
+  mongoose.connect("mongodb://127.0.0.1:27017/ysk-api", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
