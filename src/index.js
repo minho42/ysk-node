df79efa1b24a0bc32e441b30cbc4e533480bd8cb
Min ho Kim <minho42@gmail.com>
2022-08-31 11:26:37 +1000

    remove sentry related stuff for not using

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import cors from "cors";
 import morgan from "morgan";
 import cron from "node-cron";
 import helmet from "helmet";
-import Sentry from "@sentry/node";
-import Tracing from "@sentry/tracing";
 import { Currency } from "./models/currency.js";
 import { fetchAll } from "./fetchAll.js";
 fetchAll();
@@ -23,27 +21,6 @@ cron.schedule(
 );
 
 const app = express();
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  integrations: [
-    // enable HTTP calls tracing
-    new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
-    new Tracing.Integrations.Express({ app }),
-  ],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
-
-// RequestHandler creates a separate execution context using domains, so that every
-// transaction/span/breadcrumb is attached to its own Hub instance
-app.use(Sentry.Handlers.requestHandler());
-// TracingHandler creates a trace for every incoming request
-app.use(Sentry.Handlers.tracingHandler());
 
 app.use(cors());
 app.use(morgan("tiny"));
@@ -67,25 +44,10 @@ app.get("/data", async (req, res) => {
   }
 });
 
-app.get("/debug-sentry", function mainHandler(req, res) {
-  throw new Error("My first Sentry error!");
-});
-
 app.get("*", (req, res) => {
   res.status(404).send({
     error: "404 Not Found",
   });
-});
-
-// The error handler must be before any other error middleware and after all controllers
-app.use(Sentry.Handlers.errorHandler());
-
-// Optional fallthrough error handler
-app.use(function onError(err, req, res, next) {
-  // The error id is attached to `res.sentry` to be returned
-  // and optionally displayed to the user for support.
-  res.statusCode = 500;
-  res.end(res.sentry + "\n");
 });
 
 app.listen(port, () => {
